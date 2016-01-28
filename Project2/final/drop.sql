febe28b9135c718cababfbf26c9370ec195920ea
Kami <Kami@s-164-67-229-115.resnet.ucla.edu>
2016-01-28 11:47:53 -0800

    files for submission

--- Project2/final/drop.sql
+++ Project2/final/drop.sql
@@ -3,4 +3,3 @@ Drop table if exists Bids;
 Drop table if exists Bidders;
 Drop table if exists Sellers;
 Drop table if exists Categories;
-
--- Project2/shell/runload.sh
+++ Project2/shell/runload.sh
@@ -2,14 +2,13 @@
 
 # Run the drop.sql batch file to drop existing tables
 # Inside the drop.sql, you sould check whether the table exists. Drop them ONLY if they exists.
-mysql CS144 < drop.sql
+mysql TEST < drop.sql
 
 # Run the create.sql batch file to create the database and tables
-mysql CS144 < create.sql
+mysql TEST < create.sql
 
 # Compile and run the parser to generate the appropriate load files
-ant
-ant run-all
+ant -f build.xml run-all
 
 # If the Java code does not handle duplicate removal, do this now
 sort -u Items.csv -o Items.csv;
@@ -19,7 +18,7 @@ sort -u Bids.csv -o Bids.csv;
 sort -u Bidders.csv -o Bidders.csv;
 
 # Run the load.sql batch file to load the data
-mysql CS144 < load.sql
+mysql TEST < load.sql
 
 # Remove all temporary files
 rm *.csv;
